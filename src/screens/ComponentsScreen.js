ace2cc501a2d58703b0d1241bd6254e8665a8e84
jpaligorov <jpaligorov@icloud.com>
2020-11-18 13:58:31 +1100

    first commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ComponentsScreen = () => {
+  const name = "Jefe";
+
+  return (
+    <View>
+      <Text style={styles.headerStyle}>Getting started with React Native</Text>
+      <Text style={styles.subheaderStyle}>My name is {name}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    fontSize: 45,
+  },
+  subheaderStyle: {
+    fontSize: 20,
+  },
+});
+
+export default ComponentsScreen;
